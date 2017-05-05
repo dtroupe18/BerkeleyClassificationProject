2eeb3d6b7895bc403d1542f4f673b1ad91f4c67f
Dave <Dave.Troupe@gmail.com>
2017-05-05 14:20:54 -0400

    initial commit

---
+++ dataClassifier.py
@@ -0,0 +1,436 @@
+# dataClassifier.py
+# -----------------
+# Licensing Information: Please do not distribute or publish solutions to this
+# project. You are free to use and extend these projects for educational
+# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
+# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+
+# This file contains feature extraction methods and harness 
+# code for data classification
+
+import mostFrequent
+import naiveBayes
+import perceptron
+import mira
+import samples
+import sys
+import util
+
+TEST_SET_SIZE = 100
+DIGIT_DATUM_WIDTH = 28
+DIGIT_DATUM_HEIGHT = 28
+FACE_DATUM_WIDTH = 60
+FACE_DATUM_HEIGHT = 70
+
+
+def basicFeatureExtractorDigit(datum):
+    """
+    Returns a set of pixel features indicating whether
+    each pixel in the provided datum is white (0) or gray/black (1)
+    """
+    a = datum.getPixels()
+
+    features = util.Counter()
+    for x in range(DIGIT_DATUM_WIDTH):
+        for y in range(DIGIT_DATUM_HEIGHT):
+            if datum.getPixel(x, y) > 0:
+                features[(x, y)] = 1
+            else:
+                features[(x, y)] = 0
+    return features
+
+
+def basicFeatureExtractorFace(datum):
+    """
+    Returns a set of pixel features indicating whether
+    each pixel in the provided datum is an edge (1) or no edge (0)
+    """
+    a = datum.getPixels()
+
+    features = util.Counter()
+    for x in range(FACE_DATUM_WIDTH):
+        for y in range(FACE_DATUM_HEIGHT):
+            if datum.getPixel(x, y) > 0:
+                features[(x, y)] = 1
+            else:
+                features[(x, y)] = 0
+    return features
+
+
+def enhancedFeatureExtractorDigit(datum):
+    """
+    Your feature extraction playground.
+
+    You should return a util.Counter() of features
+    for this datum (datum is of type samples.Datum).
+    """
+    features = basicFeatureExtractorDigit(datum)
+    pixels = datum.getPixels()
+
+    "*** YOUR CODE HERE ***"
+    """
+    1. Percentage of pixels in the top half
+    2. Percentage of pixels in the right half
+    3. Number of non-zero pixels
+    4. Number of breaks in spaces ex: 1, 0, 1 = break
+    """
+
+    top_half = 0
+    right_half = 0
+    non_zero = 0
+    number_of_breaks = 0
+
+    for i in range(len(pixels)):
+        row = pixels[i]
+        for j in range(len(row)):
+            if row[j] != 0:
+                non_zero += 1
+                if j > 0 and row[j - 1] != row[j]:
+                    number_of_breaks += 1
+                if i <= ((len(pixels) + 1) / 2):
+                    top_half += 1
+                if j >= ((len(row) + 1) / 2):
+                    right_half += 1
+    """
+    add each feature 5 times so that it has more influence
+    basic features for the first few values are 0 anyway
+    because the first and last couple rows are all 0's
+    """
+
+    for x in range(5):
+        if top_half >= 100:
+            features[x] = 1
+        else:
+            features[x] = 0
+
+    for x in range(5):
+        if right_half >= 75:
+            features[x] = 1
+        else:
+            features[x] = 0
+
+    for x in range(5):
+        if non_zero > 300:
+            features[x] = 1
+        else:
+            features[x] = 0
+
+    for x in range(5):
+        if number_of_breaks > 175:
+            features[x] = 1
+        else:
+            features[x] = 0
+
+    return features
+
+
+def contestFeatureExtractorDigit(datum):
+    """
+    Specify features to use for the minicontest
+    """
+    features = basicFeatureExtractorDigit(datum)
+    return features
+
+
+def enhancedFeatureExtractorFace(datum):
+    """
+    Your feature extraction playground for faces.
+    It is your choice to modify this.
+    """
+    features = basicFeatureExtractorFace(datum)
+
+    """
+    I didn't write an enhanced feature extraction for the face
+    images because the performance was already good. Since the
+    problem is just binary classification all of the learning
+    algorithms performed well.
+
+    """
+    return features
+
+
+def analysis(classifier, guesses, testLabels, testData, rawTestData, printImage):
+    """
+    This function is called after learning.
+    Include any code that you want here to help you analyze your results.
+
+    Use the printImage(<list of pixels>) function to visualize features.
+
+    An example of use has been given to you.
+
+    - classifier is the trained classifier
+    - guesses is the list of labels predicted by your classifier on the test set
+    - testLabels is the list of true labels
+    - testData is the list of training datapoints (as util.Counter of features)
+    - rawTestData is the list of training datapoints (as samples.Datum)
+    - printImage is a method to visualize the features
+    (see its use in the odds ratio part in runClassifier method)
+
+    This code won't be evaluated. It is for your own optional use
+    (and you can modify the signature if you want).
+    """
+
+    # Put any code here...
+    # Example of use:
+    count = 0
+    for i in range(len(guesses)):
+        prediction = guesses[i]
+        truth = testLabels[i]
+
+        if (prediction != truth and count < 1):
+            count += 1
+            print "==================================="
+            print "Mistake on example %d" % i
+            print "Predicted %d; truth is %d" % (prediction, truth)
+            print "Image: "
+            print rawTestData[i]
+            # break
+
+
+## =====================
+## You don't have to modify any code below.
+## =====================
+
+
+class ImagePrinter:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def printImage(self, pixels):
+        """
+        Prints a Datum object that contains all pixels in the
+        provided list of pixels.  This will serve as a helper function
+        to the analysis function you write.
+
+        Pixels should take the form
+        [(2,2), (2, 3), ...]
+        where each tuple represents a pixel.
+        """
+        image = samples.Datum(None, self.width, self.height)
+        for pix in pixels:
+            try:
+                # This is so that new features that you could define which
+                # which are not of the form of (x,y) will not break
+                # this image printer...
+                x, y = pix
+                image.pixels[x][y] = 2
+            except:
+                print "new features:", pix
+                continue
+        print image
+
+
+def default(str):
+    return str + ' [Default: %default]'
+
+
+def readCommand(argv):
+    "Processes the command used to run from the command line."
+    from optparse import OptionParser
+    parser = OptionParser(USAGE_STRING)
+
+    parser.add_option('-c', '--classifier', help=default('The type of classifier'),
+                      choices=['mostFrequent', 'nb', 'naiveBayes', 'perceptron', 'mira', 'minicontest'],
+                      default='mostFrequent')
+    parser.add_option('-d', '--data', help=default('Dataset to use'), choices=['digits', 'faces'], default='digits')
+    parser.add_option('-t', '--training', help=default('The size of the training set'), default=100, type="int")
+    parser.add_option('-f', '--features', help=default('Whether to use enhanced features'), default=False,
+                      action="store_true")
+    parser.add_option('-o', '--odds', help=default('Whether to compute odds ratios'), default=False,
+                      action="store_true")
+    parser.add_option('-1', '--label1', help=default("First label in an odds ratio comparison"), default=0, type="int")
+    parser.add_option('-2', '--label2', help=default("Second label in an odds ratio comparison"), default=1, type="int")
+    parser.add_option('-w', '--weights', help=default('Whether to print weights'), default=False, action="store_true")
+    parser.add_option('-k', '--smoothing', help=default("Smoothing parameter (ignored when using --autotune)"),
+                      type="float", default=2.0)
+    parser.add_option('-a', '--autotune', help=default("Whether to automatically tune hyperparameters"), default=False,
+                      action="store_true")
+    parser.add_option('-i', '--iterations', help=default("Maximum iterations to run training"), default=3, type="int")
+    parser.add_option('-s', '--test', help=default("Amount of test data to use"), default=TEST_SET_SIZE, type="int")
+
+    options, otherjunk = parser.parse_args(argv)
+    if len(otherjunk) != 0: raise Exception('Command line input not understood: ' + str(otherjunk))
+    args = {}
+
+    # Set up variables according to the command line input.
+    print "Doing classification"
+    print "--------------------"
+    print "data:\t\t" + options.data
+    print "classifier:\t\t" + options.classifier
+    if not options.classifier == 'minicontest':
+        print "using enhanced features?:\t" + str(options.features)
+    else:
+        print "using minicontest feature extractor"
+    print "training set size:\t" + str(options.training)
+    if (options.data == "digits"):
+        printImage = ImagePrinter(DIGIT_DATUM_WIDTH, DIGIT_DATUM_HEIGHT).printImage
+        if (options.features):
+            featureFunction = enhancedFeatureExtractorDigit
+        else:
+            featureFunction = basicFeatureExtractorDigit
+        if (options.classifier == 'minicontest'):
+            featureFunction = contestFeatureExtractorDigit
+    elif (options.data == "faces"):
+        printImage = ImagePrinter(FACE_DATUM_WIDTH, FACE_DATUM_HEIGHT).printImage
+        if (options.features):
+            featureFunction = enhancedFeatureExtractorFace
+        else:
+            featureFunction = basicFeatureExtractorFace
+    else:
+        print "Unknown dataset", options.data
+        print USAGE_STRING
+        sys.exit(2)
+
+    if (options.data == "digits"):
+        legalLabels = range(10)
+    else:
+        legalLabels = range(2)
+
+    if options.training <= 0:
+        print "Training set size should be a positive integer (you provided: %d)" % options.training
+        print USAGE_STRING
+        sys.exit(2)
+
+    if options.smoothing <= 0:
+        print "Please provide a positive number for smoothing (you provided: %f)" % options.smoothing
+        print USAGE_STRING
+        sys.exit(2)
+
+    if options.odds:
+        if options.label1 not in legalLabels or options.label2 not in legalLabels:
+            print "Didn't provide a legal labels for the odds ratio: (%d,%d)" % (options.label1, options.label2)
+            print USAGE_STRING
+            sys.exit(2)
+
+    if (options.classifier == "mostFrequent"):
+        classifier = mostFrequent.MostFrequentClassifier(legalLabels)
+    elif (options.classifier == "naiveBayes" or options.classifier == "nb"):
+        classifier = naiveBayes.NaiveBayesClassifier(legalLabels)
+        classifier.setSmoothing(options.smoothing)
+        if (options.autotune):
+            print "using automatic tuning for naivebayes"
+            classifier.automaticTuning = True
+        else:
+            print "using smoothing parameter k=%f for naivebayes" % options.smoothing
+    elif (options.classifier == "perceptron"):
+        classifier = perceptron.PerceptronClassifier(legalLabels, options.iterations)
+    elif (options.classifier == "mira"):
+        classifier = mira.MiraClassifier(legalLabels, options.iterations)
+        if (options.autotune):
+            print "using automatic tuning for MIRA"
+            classifier.automaticTuning = True
+        else:
+            print "using default C=0.001 for MIRA"
+    elif (options.classifier == 'minicontest'):
+        import minicontest
+        classifier = minicontest.contestClassifier(legalLabels)
+    else:
+        print "Unknown classifier:", options.classifier
+        print USAGE_STRING
+
+        sys.exit(2)
+
+    args['classifier'] = classifier
+    args['featureFunction'] = featureFunction
+    args['printImage'] = printImage
+
+    return args, options
+
+
+USAGE_STRING = """
+  USAGE:      python dataClassifier.py <options>
+  EXAMPLES:   (1) python dataClassifier.py
+                  - trains the default mostFrequent classifier on the digit dataset
+                  using the default 100 training examples and
+                  then test the classifier on test data
+              (2) python dataClassifier.py -c naiveBayes -d digits -t 1000 -f -o -1 3 -2 6 -k 2.5
+                  - would run the naive Bayes classifier on 1000 training examples
+                  using the enhancedFeatureExtractorDigits function to get the features
+                  on the faces dataset, would use the smoothing parameter equals to 2.5, would
+                  test the classifier on the test data and performs an odd ratio analysis
+                  with label1=3 vs. label2=6
+                 """
+
+
+# Main harness code
+# TIME CHANGES
+import time
+
+
+def runClassifier(args, options):
+    start_time = time.time()
+    featureFunction = args['featureFunction']
+    classifier = args['classifier']
+    printImage = args['printImage']
+
+    # Load data
+    numTraining = options.training
+    numTest = options.test
+
+    if (options.data == "faces"):
+        rawTrainingData = samples.loadDataFile("facedata/facedatatrain", numTraining, FACE_DATUM_WIDTH,
+                                               FACE_DATUM_HEIGHT)
+        trainingLabels = samples.loadLabelsFile("facedata/facedatatrainlabels", numTraining)
+        rawValidationData = samples.loadDataFile("facedata/facedatatrain", numTest, FACE_DATUM_WIDTH, FACE_DATUM_HEIGHT)
+        validationLabels = samples.loadLabelsFile("facedata/facedatatrainlabels", numTest)
+        rawTestData = samples.loadDataFile("facedata/facedatatest", numTest, FACE_DATUM_WIDTH, FACE_DATUM_HEIGHT)
+        testLabels = samples.loadLabelsFile("facedata/facedatatestlabels", numTest)
+    else:
+        rawTrainingData = samples.loadDataFile("digitdata/trainingimages", numTraining, DIGIT_DATUM_WIDTH,
+                                               DIGIT_DATUM_HEIGHT)
+        trainingLabels = samples.loadLabelsFile("digitdata/traininglabels", numTraining)
+        rawValidationData = samples.loadDataFile("digitdata/validationimages", numTest, DIGIT_DATUM_WIDTH,
+                                                 DIGIT_DATUM_HEIGHT)
+        validationLabels = samples.loadLabelsFile("digitdata/validationlabels", numTest)
+        rawTestData = samples.loadDataFile("digitdata/testimages", numTest, DIGIT_DATUM_WIDTH, DIGIT_DATUM_HEIGHT)
+        testLabels = samples.loadLabelsFile("digitdata/testlabels", numTest)
+
+    # Extract features
+    print "Extracting features..."
+    trainingData = map(featureFunction, rawTrainingData)
+    validationData = map(featureFunction, rawValidationData)
+    testData = map(featureFunction, rawTestData)
+
+    # Conduct training and testing
+    print "Training..."
+    classifier.train(trainingData, trainingLabels, validationData, validationLabels)
+    print "Validating..."
+    guesses = classifier.classify(validationData)
+    correct = [guesses[i] == validationLabels[i] for i in range(len(validationLabels))].count(True)
+    print str(correct), ("correct out of " + str(len(validationLabels)) + " (%.1f%%).") % (
+    100.0 * correct / len(validationLabels))
+    print "Testing..."
+    guesses = classifier.classify(testData)
+    correct = [guesses[i] == testLabels[i] for i in range(len(testLabels))].count(True)
+    print str(correct), ("correct out of " + str(len(testLabels)) + " (%.1f%%).") % (100.0 * correct / len(testLabels))
+    analysis(classifier, guesses, testLabels, testData, rawTestData, printImage)
+
+    # do odds ratio computation if specified at command line
+    if ((options.odds) & (options.classifier == "naiveBayes" or (options.classifier == "nb"))):
+        label1, label2 = options.label1, options.label2
+        features_odds = classifier.findHighOddsFeatures(label1, label2)
+        if (options.classifier == "naiveBayes" or options.classifier == "nb"):
+            string3 = "=== Features with highest odd ratio of label %d over label %d ===" % (label1, label2)
+        else:
+            string3 = "=== Features for which weight(label %d)-weight(label %d) is biggest ===" % (label1, label2)
+
+        print string3
+        printImage(features_odds)
+
+    if ((options.weights) & (options.classifier == "perceptron")):
+        for l in classifier.legalLabels:
+            features_weights = classifier.findHighWeightFeatures(l)
+            print ("=== Features with high weight for label %d ===" % l)
+            printImage(features_weights)
+
+    total_time = time.time() - start_time
+    print "Total time elapsed: ", total_time, " seconds"
+
+
+if __name__ == '__main__':
+    # Read input
+    args, options = readCommand(sys.argv[1:])
+    # Run classifier
+    runClassifier(args, options)
---
+++ perceptron.py
@@ -0,0 +1,94 @@
+# perceptron.py
+# -------------
+# Licensing Information: Please do not distribute or publish solutions to this
+# project. You are free to use and extend these projects for educational
+# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
+# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+
+# Perceptron implementation
+
+import util
+
+PRINT = True
+
+
+class PerceptronClassifier:
+    """
+    Perceptron classifier.
+
+    Note that the variable 'datum' in this code refers to a counter of features
+    (not to a raw samples.Datum).
+    """
+
+    def __init__(self, legalLabels, max_iterations):
+        self.legalLabels = legalLabels
+        self.type = "perceptron"
+        self.max_iterations = max_iterations
+        self.weights = {}
+        for label in legalLabels:
+            self.weights[label] = util.Counter()  # this is the data-structure you should use
+
+    def setWeights(self, weights):
+        assert len(weights) == len(self.legalLabels);
+        self.weights = weights
+
+    def train(self, trainingData, trainingLabels, validationData, validationLabels):
+        """
+        The training loop for the perceptron passes through the training data several
+        times and updates the weight vector for each label based on classification errors.
+        See the project description for details.
+
+        Use the provided self.weights[label] data structure so that
+        the classify method works correctly. Also, recall that a
+        datum is a counter from features to values for those features
+        (and thus represents a vector a values).
+        """
+        self.features = trainingData[0].keys()
+
+        for iteration in range(self.max_iterations):
+            print "Starting iteration ", iteration, "..."
+            for i in range(len(trainingData)):
+                "*** YOUR CODE HERE ***"
+                # idea is the find the label that best represents
+                # each datum (image)
+
+                top_score = None
+                best_label = None
+                current_datum = trainingData[i] # current image
+
+                for label in self.legalLabels:
+                    # Calculate the prediction on the current image for every labels weights (0-9)
+                    result = current_datum * self.weights[label]
+                    if result > top_score or top_score is None:
+                        # save the result with the largest value - i.e most likely to be correct
+                        top_score = result
+                        best_label = label
+
+                actual_label = trainingLabels[i]
+                if best_label != actual_label: # prediction is incorrect
+                    # update weights
+                    self.weights[actual_label] = self.weights[actual_label] + current_datum # under predicted
+                    self.weights[best_label] = self.weights[best_label] - current_datum # over predicted
+
+    def classify(self, data):
+        """
+        Classifies each datum as the label that most closely matches the prototype vector
+        for that label.  See the project description for details.
+
+        Recall that a datum is a util.counter...
+        """
+        guesses = []
+        for datum in data:
+            vectors = util.Counter()
+            for l in self.legalLabels:
+                vectors[l] = self.weights[l] * datum
+            guesses.append(vectors.argMax())
+        return guesses
+
+    def findHighWeightFeatures(self, label):
+        """
+        Returns a list of the 100 features with the greatest weight for some label
+        """
+
+        return self.weights[label].sortedKeys()[:100]
